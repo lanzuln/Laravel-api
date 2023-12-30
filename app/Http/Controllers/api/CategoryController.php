b67186534f21bc5b0245d469038191dad0cf7022
lanzuln <lanzu8006@gmail.com>
2023-12-31 00:14:44 +0600

    store category done

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -2,56 +2,74 @@
 
 namespace App\Http\Controllers\api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
-class CategoryController extends Controller
-{
+class CategoryController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        $category=Category::latest('id')->get();
+    public function index() {
+        $category = Category::latest('id')->get();
         return response()->json([
-            'status'=>true,
-            'message'=>"Data retrived sussessfull",
-            'data'=>$category,
+            'status' => true,
+            'message' => "Data retrived sussessfull",
+            'data' => $category,
         ]);
     }
-
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|unique:categories',
+        ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'error',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $category = Category::create([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'category created successfully',
+            'data' => $category,
+        ]);
     }
+
+
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
+    public function show(string $id) {
         //
     }
-
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
+    public function update(Request $request, string $id) {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
+    public function destroy(string $id) {
         //
     }
 }
