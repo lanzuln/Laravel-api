c8197adb1892113aec45d3beb97f7c3778f1d01a
lanzuln <lanzu8006@gmail.com>
2023-12-30 22:44:03 +0600

    category index

--- app/Http/Controllers/categoryController.php
+++ app/Http/Controllers/categoryController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class categoryController extends Controller
 {
+    public function index(){
 
+        return response()->json([
+            'status'=>'200',
+            'message'=>"welcome to laravel api development"
+        ]);
+    }
 }
